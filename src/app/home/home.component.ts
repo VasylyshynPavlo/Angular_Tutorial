89940f0ea551cdbb88b5521ad617427f7474ddbc
VasylyshynPavlo <pavlikvstaras@gmail.com>
2024-11-06 21:15:59 +0200

    Inital

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `https://koala.sh/api/image/v2-awbqf-14wjb.jpg?width=1344&height=768&dream`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
